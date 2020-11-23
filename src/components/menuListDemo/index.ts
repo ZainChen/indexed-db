302a0fa9d8c6a1f272b36bea2d306a86b47945d2
ZainChen <2384439266@qq.com>
2020-11-23 21:00:52 +0800

    add gh-pages

--- src/components/menuListDemo/index.ts
+++
@@ -1,4 +0,0 @@
-/**
- * 防止 import 引用组件需要 '*\MenuListDemo\MenuListTest'
- */
-export { MenuListDemo } from './menuListDemo';
--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from "react-dom";
-import { Root } from 'src/components/root';
-
-render(
-    <Root />,
-    document.getElementById('root')
-);
